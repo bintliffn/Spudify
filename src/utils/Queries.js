8eda061db17d866f4842a3ffd397dec7a83dc268
bintliffn <69825921+bintliffn@users.noreply.github.com>
2022-09-07 16:42:25 -0400

    refreshtoken method working

--- src/utils/Queries.js
+++ src/utils/Queries.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const baseURL = 'https://api.spotify.com/v1/';
 
-
+//need to get access token from local storage
 export const getRecentlyPlayed = () => {
     //temporary hardcoded
     const accessToken = "BQA7xanoSd8MA9H2-9StI8KVDSwRiPVSVNx3bgIOib703_JA-g17sT7p0d77lvTAowVyH_wpsWsZWZy4fYicuZzqgC35ubDLa8oMm3aHF5hCYIxhboc6BQqWkNRieHdqu_FQ8keAK7rbsH0WNCOv2VYZl1xmY4mqs8yDShpoGTLUPOBNFVQ0NwQADVzGmki2VEKZx-8kZpUMvkE_T3K_fYoCkzY39NFN0oTxY59FgvdAw31GLV6JSf04";
--- src/utils/RefreshToken.js
+++ src/utils/RefreshToken.js
@@ -6,28 +6,29 @@ const CLIENT_SECRET = '79cd7ebaf39c4437a8418daa887b7fae';
 const querystring = require('querystring');
 const Buffer = require('buffer').Buffer;
 
-//currently only working with hard coded value (figure out why)
-export const getNewToken = (refresh_token) => {
 
-    console.log(refresh_token);
+//need to get function to store new access token (working now though)
+
+export const getNewToken = async () => {
+    let refresh_token = await SecureStore.getItemAsync("refresh_token");
+    var refresh_token_prepped = JSON.parse(refresh_token);
     axios({
         method: 'post',
         url: 'https://accounts.spotify.com/api/token',
         data: querystring.stringify({
           grant_type : 'refresh_token',
-          refresh_token: refresh_token
+          refresh_token: refresh_token_prepped
         }),
         headers: {
           Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
           'Content-Type': 'application/x-www-form-urlencoded'
         },
       }) //handle the response
-        .then(response => {
-          console.log("CALLED\n");
-          console.log(response);
+        .then(async response => {
+          await SecureStore.setItemAsync("access_token", response.data.access_token);
         })
         .catch(error => {
-          console.log(error);
+          console.log(error.response.data);
         });
 
 }
