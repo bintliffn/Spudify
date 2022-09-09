b09d86de1e174d7afb7268112f8729b929f052a3
bintliffn <69825921+bintliffn@users.noreply.github.com>
2022-09-09 16:39:32 -0400

    All API queries added currently work

--- src/screens/Test.js
+++ src/screens/Test.js
@@ -1,10 +1,13 @@
-import { getTopArtistsOrSongs } from "../utils/Queries";
+import { getTopArtistsOrSongs, getTopArtistsOrTracks, getAudioAnalysisOfSong, getRecommendationsArtists } from "../utils/Queries";
 import { Button, SafeAreaView, View } from 'react-native';
+
+const artists = ['4YjpqCSDD7zwMQgPYJMqb0']
 
 const Test = ({ navigation })=>{
     return (
         <SafeAreaView>
-          <Button title = 'test' onPress={() => (getTopArtistsOrSongs('artists'))}/>
+          <Button title = 'test' onPress={() => (getRecommendationsArtists("4YjpqCSDD7zwMQgPYJMqb0,7Hvq85OU8T7Hsd63zNBwaL,"+
+          "2wXUKlYvdBHn2MNeRKgG6W","","",5))}/>
         </SafeAreaView>
     );
 }
--- src/utils/Queries.js
+++ src/utils/Queries.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 
 const baseURL = 'https://api.spotify.com/v1/';
 
-export default async function getRecentlyPlayed() {
+
+
+//Working
+export async function getRecentlyPlayed() {
     const accessToken = await SecureStore.getItemAsync("access_token");
       axios({
         method: 'get',
@@ -22,9 +25,9 @@ export default async function getRecentlyPlayed() {
         });
 }
 //must pass either "tracks" or "artists" for function to work (retreives top artists or tracks for a user)
-//also must pass time_range (valid values are long_term, medium_term, short_term)
-//Need to test
-export default async function getTopArtistsOrTracks(artistsOrTracks, time_range) {
+//also must pass time_range (valid values are long_term, medium_term, short_term) and limit (1-50)
+//working
+export async function getTopArtistsOrTracks(artistsOrTracks, time_range, limit) {
   const accessToken = await SecureStore.getItemAsync("access_token");
     axios({
       method: 'get',
@@ -32,19 +35,23 @@ export default async function getTopArtistsOrTracks(artistsOrTracks, time_range)
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer '+accessToken,
-        'time_range': `${time_range}`
       },
+      params :{
+        'time_range': `${time_range}`,
+        'limit': `${limit}`
+      }
     }) //handle the response
       .then(response => {
         //returns array of songs/artists (array contains a lot of information about the song/artist that will need to be extracted in a later step)
-          return response.data.items;
+        return response.data.items;
       })
       .catch(error => {
         console.log(error);
       });
 }
-//need to test
-export default async function getAudioAnalysisOfSong (trackId) {
+
+//working
+export async function getAudioAnalysisOfSong (trackId) {
   const accessToken = await SecureStore.getItemAsync("access_token");
     axios({
       method: 'get',
@@ -55,8 +62,37 @@ export default async function getAudioAnalysisOfSong (trackId) {
       },
     }) //handle the response
       .then(response => {
-        //returns array of songs/artists (array contains a lot of information about the song/artist that will need to be extracted in a later step)
-          return response.data;
+        //returns audio analysis of the track
+        return response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+}
+
+//Working
+//returns recommendations based off of up to 5 artists/songs/genres (any combination of the 3 up to 5 parameters total) 
+//(artists or  songs must be passed as their Spotify Id's)
+//Artists/Genres/Tracks must be passed as comma seperated values ex. track1,track2,track3
+export async function getRecommendations (artists,genres,tracks, limit) {
+  const accessToken = await SecureStore.getItemAsync("access_token");
+    axios({
+      method: 'get',
+      url: `${baseURL}recommendations`,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+accessToken,
+      },
+      params :{
+        'seed_artists': artists,
+        'seed_genres' : genres,
+        'seed_tracks' : tracks,
+        'limit': limit,
+      }
+    }) //handle the response
+      .then(response => {
+        //returns array of songs(array contains a lot of information about the song/artist that will need to be extracted in a later step)
+        return response.data.tracks;
       })
       .catch(error => {
         console.log(error);
