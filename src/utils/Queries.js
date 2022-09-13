ec262239df2f658786bd62d08d956f639a52e7d5
bintliffn <69825921+bintliffn@users.noreply.github.com>
2022-09-13 13:25:24 -0400

    Added comments to api queries

--- src/utils/Queries.js
+++ src/utils/Queries.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 
 const baseURL = 'https://api.spotify.com/v1/';
 
-//Working
+//Returns an array of 20 most recently played tracks
 export async function getRecentlyPlayed() {
+  //retreive access token from local storage
     const accessToken = await SecureStore.getItemAsync("access_token");
+    //send GET request to spotify
       axios({
         method: 'get',
         url: `${baseURL}me/player/recently-played`,
@@ -15,16 +17,16 @@ export async function getRecentlyPlayed() {
         },
       }) //handle the response
         .then(response => {
-          //returns array of songs (array contains a lot of information about the song that will need to be extracted in a later step)
+          //returns array of tracks in JSON object format
             return response.data.items;
         })
         .catch(error => {
           console.log(error);
         });
 }
+
 //must pass either "tracks" or "artists" for function to work (retreives top artists or tracks for a user)
 //also must pass time_range (valid values are long_term, medium_term, short_term) and limit (1-50)
-//working
 export async function getTopArtistsOrTracks(artistsOrTracks, time_range, limit) {
   const accessToken = await SecureStore.getItemAsync("access_token");
     axios({
@@ -40,7 +42,7 @@ export async function getTopArtistsOrTracks(artistsOrTracks, time_range, limit) 
       }
     }) //handle the response
       .then(response => {
-        //returns array of songs/artists depending on value passed in
+        //returns array of tracks/artists depending on value passed in
         return response.data.items;
       })
       .catch(error => {
@@ -48,7 +50,8 @@ export async function getTopArtistsOrTracks(artistsOrTracks, time_range, limit) 
       });
 }
 
-//working
+//returns attribute values for a song (ex. dancability: .67)
+//must pass in a trackid
 export async function getAudioAnalysisOfSong (trackId) {
   const accessToken = await SecureStore.getItemAsync("access_token");
     axios({
@@ -68,10 +71,12 @@ export async function getAudioAnalysisOfSong (trackId) {
       });
 }
 
-//Working
 //returns recommendations based off of up to 5 artists/songs/genres (any combination of the 3 up to 5 parameters total) 
+//You can pass empty strings if you do not want to use any combination of artists/songs/genres
 //(artists or  songs must be passed as their Spotify Id's)
+//genres are passes as a tring
 //Artists/Genres/Tracks must be passed as comma seperated values ex. track1,track2,track3
+//limit is the number of recommended tracks you want to recieve (max: 50)
 export async function getRecommendations (artists,genres,tracks, limit) {
   const accessToken = await SecureStore.getItemAsync("access_token");
     axios({
@@ -89,7 +94,7 @@ export async function getRecommendations (artists,genres,tracks, limit) {
       }
     }) //handle the response
       .then(response => {
-        //returns array of songs
+        //returns array of songs in JSON object
         return response.data.tracks;
       })
       .catch(error => {
@@ -99,6 +104,7 @@ export async function getRecommendations (artists,genres,tracks, limit) {
 
 //Working
 //returns recommendations. The only parameter is a JSON object containing the parameters that should be added to the request
+//This function will mainly be used in phase 2 when advanced recommendations can be used
 export async function getRecommendationsAdvanced (jsonBody) {
   const accessToken = await SecureStore.getItemAsync("access_token");
     axios({
