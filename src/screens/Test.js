0ec42c651d508f1f3e5648b7b018671697581dbe
bintliffn <69825921+bintliffn@users.noreply.github.com>
2022-09-18 15:49:35 -0400

    Song component prototype is finished

--- src/screens/Test.js
+++ src/screens/Test.js
@@ -5,8 +5,13 @@ import {
   getRecommendationsAdvanced,
   getRecentlyPlayed,
 } from "../utils/Queries";
-import { Button, SafeAreaView, View } from "react-native";
-import Song from "../Components/Song";
+import { Button, SafeAreaView, View , Text} from "react-native";
+import Song from "../Components/DisplaySong/Song";
+import React from "react";
+import { StyleSheet } from "react-native";
+
+
+
 
 const payload = {
   seed_artists:
@@ -18,15 +23,53 @@ const payload = {
 
 async function testFunc() {
   const test = await getTopArtistsOrTracks("tracks", "long_term", 5);
-  Song(test[0]);
+  setJsonBody(test[0]);
+  setDisplay(true)
 }
 
 const Test = ({ navigation }) => {
+  const [display, setDisplay] = React.useState(false);
+  const[jsonBody,setJsonBody] = React.useState();
+
+  async function testFunc() {
+    const test = await getRecentlyPlayed(5);
+    setJsonBody(test[0].track);
+    setDisplay(true)
+  }
+
+  React.useEffect(()=>{
+      testFunc();
+  },[])
+
   return (
+    //work on displaying full songComponent
     <SafeAreaView>
-      <Button title="test" onPress={() => testFunc()} />
+      {display ?
+      ( 
+      <View style={[styles.view]}>
+      <Song SingleJsonSong={jsonBody} style={[styles.song]}/>
+      <Song SingleJsonSong={jsonBody} style={[styles.song]}/>
+      <Song SingleJsonSong={jsonBody} style={[styles.song]}/>
+         </View>):
+         (<View>
+            <Text >
+              RENDERING DATA
+              </Text>
+         </View>)}
+
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  song:{
+    padding:10,
+    margin: 100
+},
+view:{
+  justifyContent: "center",
+  alignItems: 'center'
+}
+});
 
 export default Test;
